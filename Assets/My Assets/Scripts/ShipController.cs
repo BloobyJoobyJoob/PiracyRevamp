8d0c0b8a6fd9243aee049d4a2382707c21fbfffc
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2022-09-04 14:24:36 +1200

    Added Particle System white wash

--- Assets/My Assets/Scripts/ShipController.cs
+++ Assets/My Assets/Scripts/ShipController.cs
@@ -12,17 +12,30 @@ public class ShipController : MonoBehaviour
     public float moveForce;
     public float rotateForce;
 
+    public ParticleSystem ps;
+    public float spawnMultiplier;
+    public float spawnMin;
+
     private Vector2 movement = Vector2.zero;
+
+    private ParticleSystem.EmissionModule em;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+
+        em = ps.emission;
     }
 
     private void FixedUpdate()
     {
         rb.AddForce(Mathf.Abs(movement.y) * moveForce * transform.forward, ForceMode.Acceleration);
         rb.AddTorque(movement.x * rotateForce * new Vector2(rb.velocity.x, rb.velocity.z).sqrMagnitude * -transform.up, ForceMode.Acceleration);
+    }
+
+    private void Update()
+    {
+        em.rateOverTime = spawnMin + (rb.velocity.sqrMagnitude * spawnMultiplier);
     }
 
     public void OnMove(InputAction.CallbackContext context)
